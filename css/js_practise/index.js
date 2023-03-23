8199a9e57317e69bef6c7bc8e2ed6c0c4a0f1a4b
Prashant Mishra <prashantmishra111@outlook.com>
2023-03-24 03:58:12 +0530

    html canvas!

--- css/js_practise/index.js
+++ css/js_practise/index.js
@@ -3,11 +3,10 @@
 // const api = " https://disease.sh/v3/covid-19/countries";
 
 // let myPromise = new Promise((resolve, reject) => {
-    
+
 //     let data; //synchronusly chalega.
 
 //    fetch(api).then((res) =>  res.json()).then((dta) => data =  dta); //asynchronous aur future run hogi. seven number line promise ke ander aa raha hai future me ans ayega.
-
 
 //   resolve(data)
 //   console.log(data)
@@ -24,13 +23,14 @@
 let name = " prashant";
 const api = " https://disease.sh/v3/covid-19/countries";
 
- let mpromise = new Promise((reject, resolve)=>{
- reject(40) // js will always take 1st parameter as resolve
-            //  and second parameter as reject
-resolve(12)
- }) 
+let mpromise = new Promise((reject, resolve) => {
+  reject(40); // js will always take 1st parameter as resolve
+  //  and second parameter as reject
+  resolve(12);
+});
 
- mpromise.then((res) => console.log(res))
- .catch((err) => console.log(err, "err"))
+mpromise
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err, "err"));
 
- console.log("kya ageka code break hoga...")
+console.log("kya ageka code break hoga...");
