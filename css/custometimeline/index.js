5151a79f304eb2d2e5eed4b4b246aab9af172bdc
Prashant Mishra <prashantmishra111@outlook.com>
2023-05-06 23:59:44 +0530

    !

--- css/custometimeline/index.js
+++ css/custometimeline/index.js
@@ -1,34 +1,5 @@
 var items = document.querySelectorAll("li");
-const coursecurriculum = [
-  {
-    title: "1st Month",
-    contentTitle: "Introduction to programming",
-    contentText: `-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nun
-          -vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`,
-    // contentDetailedText: `On 10 May 1940, Hitler began his long-awaited offensive in the west by invading neutral Holland and Belgium and attacking northern France. Holland capitulated after only five days of fighting, and the Belgians surrendered on 28 May.  `,
-  },
-  {
-    title: "2nd Month",
-    contentTitle: "Data Structures and Algorithm",
-    contentText: `-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nun
-          -vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`,
-    // contentDetailedText: `On 10 May 1940, Hitler began his long-awaited offensive in the west by invading neutral Holland and Belgium and attacking northern France. Holland capitulated after only five days of fighting, and the Belgians surrendered on 28 May.  `,
-  },
-  {
-    title: "3rd Month",
-    contentTitle: "Data Structures and Algorithm",
-    contentText: `-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nun
-          -vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`,
-    // contentDetailedText: `On 10 May 1940, Hitler began his long-awaited offensive in the west by invading neutral Holland and Belgium and attacking northern France. Holland capitulated after only five days of fighting, and the Belgians surrendered on 28 May.  `,
-  },
-  {
-    title: "4th Month",
-    contentTitle: "Data Structures and Algorithm",
-    contentText: `-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nun
-          -vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`,
-    // contentDetailedText: `On 10 May 1940, Hitler began his long-awaited offensive in the west by invading neutral Holland and Belgium and attacking northern France. Holland capitulated after only five days of fighting, and the Belgians surrendered on 28 May.  `,
-  },
-];
+ console.log(items, "items");
 function isItemInView(item) {
   var rect = item.getBoundingClientRect();
   return (
