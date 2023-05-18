473e812ab394ec791db5b10c565281a6ea320dec
Prashant Mishra <prashantmishra111@outlook.com>
2023-05-18 21:26:28 +0530

    select item using checkbox.

---
+++ stopwatch/app/src/component/CheckboxSelect.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "../App.css";
+/*
+Q. you have array of items and you have to select the items and show the selected items in a list.
+ you have to use checkbox to select the items.
+
+*/
+function CheckboxSelect() {
+  const [myArmor, setMyArmor] = React.useState(() => []); // ["Mark I", "Mark II"
+  const armorOptions = [
+    { id: 1, armor: "Mark I" },
+    { id: 2, armor: "Mark II" },
+    { id: 3, armor: "Mark III" },
+    { id: 4, armor: "Mark IV" },
+    { id: 5, armor: "Mark V" },
+    { id: 6, armor: "Mark VI" },
+  ];
+
+  const handleArmorSelect = (id, isChecked) => {
+    console.log(id, isChecked, "----");
+    if (isChecked) {
+      const selectedArmor = armorOptions.filter((ele) => {
+        return ele.id === id;
+      });
+      const [armor] = selectedArmor;
+      setMyArmor((prev) => [...prev, armor]);
+    } else if (isChecked === false) {
+      const selectedArmor = myArmor.filter((ele) => {
+        return ele.id !== id;
+      });
+
+      console.log(selectedArmor, "filtered armor..");
+
+      setMyArmor(selectedArmor);
+    }
+  };
+
+  console.log(myArmor, "myArmor");
+  return (
+    <div className="App">
+      <h1>Select Armor </h1>
+      {armorOptions.map((ele) => {
+        return (
+          <div key={ele.id}>
+            <span>
+              <input
+                type="checkbox"
+                name="select"
+                id="selectArmor"
+                onClick={(e) => handleArmorSelect(ele.id, e.target.checked)}
+              />
+            </span>{" "}
+            <span>{ele.armor}</span>
+          </div>
+        );
+      })}
+
+      <div>
+        <h1>Selected Armor : </h1>
+
+        {myArmor && myArmor.length === 0 ? (
+          <h2>No Armor Selected </h2>
+        ) : myArmor && myArmor.length > 0 ? (
+          myArmor.map((ele) => <h2 key={ele.id}>{ele.armor}</h2>)
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
+export default CheckboxSelect;
--- stopwatch/app/src/index.js
+++ stopwatch/app/src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import FetchData from './FetchData';
  import StarProb from "./component/StarProb";
-
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(
-   <React.StrictMode>
-     {/* <App /> */}
-     {/* <FetchData /> */}
-     <StarProb />
-   </React.StrictMode>
- );
+import CheckboxSelect from "./component/CheckboxSelect";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <FetchData /> */}
+    {/* <StarProb /> */}
+    <CheckboxSelect />
+  </React.StrictMode>
+);
 
  
