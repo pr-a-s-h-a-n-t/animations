ace00491420b91e8ea014ed3a99ab895f9502b7d
Prashant Mishra <prashantmishra111@outlook.com>
2023-05-19 20:00:38 +0530

    animation ad

--- css/custometimeline/index.js
+++ css/custometimeline/index.js
@@ -1,7 +1,19 @@
 var items = document.querySelectorAll("li");
  console.log(items, "items");
+
+// isItemInView
 function isItemInView(item) {
   var rect = item.getBoundingClientRect();
+  console.log(rect, "rect");
+
+  console.log(
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth),  
+  )
+
   return (
     rect.top >= 0 &&
     rect.left >= 0 &&
@@ -11,6 +23,8 @@ function isItemInView(item) {
   );
 }
 
+
+// callbackFunc call 
 function callbackFunc() {
   for (var i = 0; i < items.length; i++) {
     if (isItemInView(items[i])) {
